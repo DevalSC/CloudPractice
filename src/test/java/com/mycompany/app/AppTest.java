d237dbaeee6c78d7016b4efe2a5b43937eb42849
Deval Sandip Chaudhari <114732760+DevalSC@users.noreply.github.com>
2023-10-20 15:46:49 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Hello Welcome to My repo", app.getMessage());
     }
 }
